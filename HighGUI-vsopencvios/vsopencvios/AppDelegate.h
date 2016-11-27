ba90464af13f3716fccb302d1c107086137ee85a
Virendra Shakya <virendra.shakya@chartboost.com>
2016-11-26 20:37:56 -0800

    iphone code

---
+++ HighGUI-vsopencvios/vsopencvios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  vsopencvios
+//
+//  Created by Virendra Shakya on 11/18/16.
+//  Copyright © 2016 Virendra Shakya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HighGUI-vsopencvios/vsopencvios/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  vsopencvios
+//
+//  Created by Virendra Shakya on 11/18/16.
+//  Copyright © 2016 Virendra Shakya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
