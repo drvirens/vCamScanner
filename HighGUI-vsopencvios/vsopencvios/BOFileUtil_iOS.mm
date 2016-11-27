c4ba1c85c8a83048f90f3c04629f94d7af81067b
drvirens <drvirens@gmail.com>
2016-11-27 01:36:15 -0800

    modified some files

--- HighGUI-vsopencvios/vsopencvios/BOFileUtil_iOS.mm
+++ HighGUI-vsopencvios/vsopencvios/BOFileUtil_iOS.mm
@@ -11,20 +11,10 @@
 #include "BOFileUtil_iOS.hpp"
 
 void BOFileUtil_iOS::init() {
-	string s = getWritablePath();
-	cameraDirectory_ = s.c_str();
+	cameraDirectory_ = getWritablePath();
 }
 
 string BOFileUtil_iOS::getWritablePath() const {
-//	if (!cameraDirectory_) {
-//
-//	}
-//	string p = cameraDirectory_;
-//	if (p.length())
-//		{
-//		return p;
-//		}
-//	
 		// save to document folder
 	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
 	NSString *documentsDirectory = [paths objectAtIndex:0];
