278a8840d0455370d955f758df0505ed5df32e27
Virendra Shakya <virendra.shakya@chartboost.com>
2016-11-28 23:00:12 -0800

    more GUI changes

---
+++ HighGUI-vsopencvios/vsopencvios/BOEnterDetailsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BOEnterDetailsTableViewController.h
+//  vsopencvios
+//
+//  Created by Virendra Shakya on 11/28/16.
+//  Copyright © 2016 Virendra Shakya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BOEnterDetailsTableViewController : UITableViewController
+
+@end
