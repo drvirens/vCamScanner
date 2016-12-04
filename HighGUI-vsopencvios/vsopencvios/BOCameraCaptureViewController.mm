816cad8cc2baeb6123b17c9c143bbfa09b611e23
Virendra Shakya <virendra.shakya@chartboost.com>
2016-12-03 20:45:48 -0800

    separate out some of opencv processing in separate c++ class for reuse

--- HighGUI-vsopencvios/vsopencvios/BOCameraCaptureViewController.mm
+++ HighGUI-vsopencvios/vsopencvios/BOCameraCaptureViewController.mm
@@ -6,6 +6,9 @@
 //  Copyright © 2016 Virendra Shakya. All rights reserved.
 //
 
+#include "vsImageProcessor.hpp"
+#include "trace.h"
+
 #import "BOCameraCaptureViewController.h"
 #import "BOCameraController.h"
 #import "MMCropView.h"
@@ -761,8 +764,8 @@ static void* gUserLoadContext = &gUserLoadContext;
     std::vector<std::vector<cv::Point>>squares;
     std::vector<cv::Point> largest_square;
     
-    find_squares(original, squares);
-    find_largest_square(squares, largest_square);
+    vsImageProcessor::find_squares(original, squares);
+    vsImageProcessor::find_largest_square(squares, largest_square);
     
     if (largest_square.size() == 4)
     {
@@ -840,183 +843,32 @@ static void* gUserLoadContext = &gUserLoadContext;
     original.release();
 }
 
-// http://stackoverflow.com/questions/8667818/opencv-c-obj-c-detecting-a-sheet-of-paper-square-detection
-static void find_squares(cv::Mat& image, std::vector<std::vector<cv::Point>>&squares) {
-    
-    // blur will enhance edge detection
-    
-    cv::Mat blurred(image);
-    //    medianBlur(image, blurred, 9);
-    GaussianBlur(image, blurred, cvSize(11,11), 0);//change from median blur to gaussian for more accuracy of square detection
-    
-    cv::Mat gray0(blurred.size(), CV_8U), gray;
-    std::vector<std::vector<cv::Point> > contours;
-    
-    // find squares in every color plane of the image
-    for (int c = 0; c < 3; c++)
-    {
-        int ch[] = {c, 0};
-        mixChannels(&blurred, 1, &gray0, 1, ch, 1);
-        
-        // try several threshold levels
-        const int threshold_level = 2;
-        for (int l = 0; l < threshold_level; l++)
-        {
-            // Use Canny instead of zero threshold level!
-            // Canny helps to catch squares with gradient shading
-            if (l == 0)
-            {
-                Canny(gray0, gray, 10, 20, 3); //
-                //                Canny(gray0, gray, 0, 50, 5);
-                
-                // Dilate helps to remove potential holes between edge segments
-                dilate(gray, gray, cv::Mat(), cv::Point(-1,-1));
-            }
-            else
-            {
-                gray = gray0 >= (l+1) * 255 / threshold_level;
-            }
-            
-            // Find contours and store them in a list
-            findContours(gray, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
-            
-            // Test contours
-            std::vector<cv::Point> approx;
-            for (size_t i = 0; i < contours.size(); i++)
-            {
-                // approximate contour with accuracy proportional
-                // to the contour perimeter
-                approxPolyDP(cv::Mat(contours[i]), approx, arcLength(cv::Mat(contours[i]), true)*0.02, true);
-                
-                // Note: absolute value of an area is used because
-                // area may be positive or negative - in accordance with the
-                // contour orientation
-                if (approx.size() == 4 &&
-                    fabs(contourArea(cv::Mat(approx))) > 1000 &&
-                    isContourConvex(cv::Mat(approx)))
-                {
-                    double maxCosine = 0;
-                    
-                    for (int j = 2; j < 5; j++)
-                    {
-                        double cosine = fabs(angle(approx[j%4], approx[j-2], approx[j-1]));
-                        maxCosine = MAX(maxCosine, cosine);
-                    }
-                    
-                    if (maxCosine < 0.3)
-                        squares.push_back(approx);
-                }
-            }
-        }
-    }
-}
-
-static void find_largest_square(const std::vector<std::vector<cv::Point> >& squares, std::vector<cv::Point>& biggest_square)
-{
-    if (!squares.size())
-    {
-        // no squares detected
-        return;
-    }
-    
-    int max_width = 0;
-    int max_height = 0;
-    int max_square_idx = 0;
-    
-    for (size_t i = 0; i < squares.size(); i++)
-    {
-        // Convert a set of 4 unordered Points into a meaningful cv::Rect structure.
-        cv::Rect rectangle = boundingRect(cv::Mat(squares[i]));
-
-        // Store the index position of the biggest square found
-        if ((rectangle.width >= max_width) && (rectangle.height >= max_height))
-        {
-            max_width = rectangle.width;
-            max_height = rectangle.height;
-            max_square_idx = i;
-        }
-    }
-    
-    biggest_square = squares[max_square_idx];
-}
-
-static double angle( cv::Point pt1, cv::Point pt2, cv::Point pt0 ) {
-    double dx1 = pt1.x - pt0.x;
-    double dy1 = pt1.y - pt0.y;
-    double dx2 = pt2.x - pt0.x;
-    double dy2 = pt2.y - pt0.y;
-    return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
-}
-
-static cv::Mat debugSquares( std::vector<std::vector<cv::Point> > squares, cv::Mat image ){
-    NSLog(@"DEBUG!/?!");
-    for ( unsigned int i = 0; i< squares.size(); i++ ) {
-        // draw contour
-        NSLog(@"LOOP!");
-        cv::drawContours(image, squares, i, cv::Scalar(255,0,0), 1, 8, std::vector<cv::Vec4i>(), 0, cv::Point());
-        
-        // draw bounding rect
-        cv::Rect rect = boundingRect(cv::Mat(squares[i]));
-        cv::rectangle(image, rect.tl(), rect.br(), cv::Scalar(0,255,0), 2, 8, 0);
-        
-        // draw rotated rect
-        cv::RotatedRect minRect = minAreaRect(cv::Mat(squares[i]));
-        cv::Point2f rect_points[4];
-        minRect.points( rect_points );
-        for ( int j = 0; j < 4; j++ ) {
-            cv::line( image, rect_points[j], rect_points[(j+1)%4], cv::Scalar(0,0,255), 1, 8 ); // blue
-        }
-    }
-    return image;
-}
-
 - (void)doCropImage {
     if([self.croppedView frameEdited]){
-        CGFloat scaleFactor =  [self.capturedImageView contentScale];
-        CGPoint ptBottomLeft = [self.croppedView coordinatesForPoint:1 withScaleFactor:scaleFactor];
-        CGPoint ptBottomRight = [self.croppedView coordinatesForPoint:2 withScaleFactor:scaleFactor];
-        CGPoint ptTopRight = [self.croppedView coordinatesForPoint:3 withScaleFactor:scaleFactor];
-        CGPoint ptTopLeft = [self.croppedView coordinatesForPoint:4 withScaleFactor:scaleFactor];
+        double scaleFactor =  [self.capturedImageView contentScale];
         
-        CGFloat w1 = sqrt( pow(ptBottomRight.x - ptBottomLeft.x , 2) + pow(ptBottomRight.x - ptBottomLeft.x, 2));
-        CGFloat w2 = sqrt( pow(ptTopRight.x - ptTopLeft.x , 2) + pow(ptTopRight.x - ptTopLeft.x, 2));
-        CGFloat h1 = sqrt( pow(ptTopRight.y - ptBottomRight.y , 2) + pow(ptTopRight.y - ptBottomRight.y, 2));
-        CGFloat h2 = sqrt( pow(ptTopLeft.y - ptBottomLeft.y , 2) + pow(ptTopLeft.y - ptBottomLeft.y, 2));
+        CGPoint thePtBottomLeft = [self.croppedView coordinatesForPoint:1 withScaleFactor:scaleFactor];
+        CGPoint thePtBottomRight = [self.croppedView coordinatesForPoint:2 withScaleFactor:scaleFactor];
+        CGPoint thePtTopRight = [self.croppedView coordinatesForPoint:3 withScaleFactor:scaleFactor];
+        CGPoint thePtTopLeft = [self.croppedView coordinatesForPoint:4 withScaleFactor:scaleFactor];
         
-        CGFloat maxWidth = (w1 < w2) ? w1 : w2;
-        CGFloat maxHeight = (h1 < h2) ? h1 : h2;
-        
-        cv::Point2f src[4], dst[4];
-        src[0].x = ptTopLeft.x;
-        src[0].y = ptTopLeft.y;
-        src[1].x = ptTopRight.x;
-        src[1].y = ptTopRight.y;
-        src[2].x = ptBottomRight.x;
-        src[2].y = ptBottomRight.y;
-        src[3].x = ptBottomLeft.x;
-        src[3].y = ptBottomLeft.y;
-        
-        dst[0].x = 0;
-        dst[0].y = 0;
-        dst[1].x = maxWidth - 1;
-        dst[1].y = 0;
-        dst[2].x = maxWidth - 1;
-        dst[2].y = maxHeight - 1;
-        dst[3].x = 0;
-        dst[3].y = maxHeight - 1;
-        
-        cv::Mat undistorted = cv::Mat( cvSize(maxWidth,maxHeight), CV_8UC4);
+        cv::Mat* undistorted = 0; //output
         cv::Mat original = [MMOpenCVHelper cvMatFromUIImage:self.image];
         
-        NSLog(@"%f %f %f %f",ptBottomLeft.x,ptBottomRight.x,ptTopRight.x,ptTopLeft.x);
-        cv::warpPerspective(original, undistorted, cv::getPerspectiveTransform(src, dst), cvSize(maxWidth, maxHeight));
+        vsCGPoint ptBottomLeft = {thePtBottomLeft.x, thePtBottomLeft.y};
+        vsCGPoint ptBottomRight = {thePtBottomRight.x, thePtBottomRight.y};
+        vsCGPoint ptTopRight = {thePtTopRight.x, thePtTopRight.y};
+        vsCGPoint ptTopLeft = {thePtTopLeft.x, thePtTopLeft.y};
         
-        self.capturedImageView.image=[MMOpenCVHelper UIImageFromCVMat:undistorted];
+        vsImageProcessor::crop(original, scaleFactor, ptBottomLeft, ptBottomRight, ptTopRight, ptTopLeft, undistorted);
+        
+        self.capturedImageView.image=[MMOpenCVHelper UIImageFromCVMat:*undistorted];
         self.cropImage = self.capturedImageView.image;
         self.croppedView.hidden = YES;
         
         original.release();
-        undistorted.release();
+        undistorted->release();
+        delete undistorted;
     }
     else{
         UIAlertView  *alertView = [[UIAlertView alloc] initWithTitle:@"MMCamScanner" message:@"Invalid Rect" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
