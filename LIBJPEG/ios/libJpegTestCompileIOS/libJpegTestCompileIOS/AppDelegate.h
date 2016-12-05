d69acdca1f5c820a908d7a2cc90abf925215341e
Virendra Shakya <virendra.shakya@chartboost.com>
2016-12-04 20:07:52 -0800

    compile libjpeg for iOS

---
+++ LIBJPEG/ios/libJpegTestCompileIOS/libJpegTestCompileIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  libJpegTestCompileIOS
+//
+//  Created by Virendra Shakya on 12/4/16.
+//  Copyright © 2016 Virendra Shakya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LIBJPEG/ios/libJpegTestCompileIOS/libJpegTestCompileIOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  libJpegTestCompileIOS
+//
+//  Created by Virendra Shakya on 12/4/16.
+//  Copyright © 2016 Virendra Shakya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
