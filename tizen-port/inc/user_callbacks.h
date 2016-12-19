5794222042485101978946aaf35ec2ec92929d45
Virendra Shakya <virendra.shakya@chartboost.com>
2016-12-19 12:08:22 -0800

    camera working

---
+++ tizen-port/inc/user_callbacks.h
@@ -0,0 +1,14 @@
+
+
+#ifndef USR_CB_H
+#define USR_CB_H
+
+
+#include "main.h"
+
+Evas_Object*  create_buttons_in_main_window(appdata_s *ad, Evas_Object *conform);
+void camera_pop_cb();
+int startCamera(appdata_s *ad);
+
+
+#endif                           /* USR_CB_H */
--- tizen-port/src/vcamscannertizen.c
+++ tizen-port/src/vcamscannertizen.c
@@ -1,173 +1,134 @@
+
 #include "vcamscannertizen.h"
+#include "user_callbacks.h"
 
-typedef struct appdata {
-	Evas_Object *win;
-	Evas_Object *conform;
-	Evas_Object *label;
-} appdata_s;
+Evas_Object *GLOBAL_DEBUG_BOX;
 
-static void
-win_delete_request_cb(void *data, Evas_Object *obj, void *event_info)
+void _add_entry_text(const char *text)
 {
-	ui_app_exit();
+    Evas_Coord c_y;
+
+    elm_entry_entry_append(GLOBAL_DEBUG_BOX, text);
+    elm_entry_entry_append(GLOBAL_DEBUG_BOX, "<br>");
+    elm_entry_cursor_end_set(GLOBAL_DEBUG_BOX);
+    elm_entry_cursor_geometry_get(GLOBAL_DEBUG_BOX, NULL, &c_y, NULL, NULL);
+    elm_scroller_region_show(GLOBAL_DEBUG_BOX, 0, c_y, 0, 0);
 }
 
-static void
-win_back_cb(void *data, Evas_Object *obj, void *event_info)
+static Eina_Bool _pop_cb(void *data, Elm_Object_Item *item)
 {
-	appdata_s *ad = data;
-	/* Let window go to hide state. */
-	elm_win_lower(ad->win);
+    elm_win_lower(((appdata_s *)data)->win);
+    return EINA_FALSE;
 }
 
-static void
-create_base_gui(appdata_s *ad)
+Evas_Object *_new_button(void *data, Evas_Object *display, char *name, void *cb)
 {
-	/* Window */
-	/* Create and initialize elm_win.
-	   elm_win is mandatory to manipulate window. */
-	ad->win = elm_win_util_standard_add(PACKAGE, PACKAGE);
-	elm_win_autodel_set(ad->win, EINA_TRUE);
-
-	if (elm_win_wm_rotation_supported_get(ad->win)) {
-		int rots[4] = { 0, 90, 180, 270 };
-		elm_win_wm_rotation_available_rotations_set(ad->win, (const int *)(&rots), 4);
-	}
-
-	evas_object_smart_callback_add(ad->win, "delete,request", win_delete_request_cb, NULL);
-	eext_object_event_callback_add(ad->win, EEXT_CALLBACK_BACK, win_back_cb, ad);
-
-	/* Conformant */
-	/* Create and initialize elm_conformant.
-	   elm_conformant is mandatory for base gui to have proper size
-	   when indicator or virtual keypad is visible. */
-	ad->conform = elm_conformant_add(ad->win);
-	elm_win_indicator_mode_set(ad->win, ELM_WIN_INDICATOR_SHOW);
-	elm_win_indicator_opacity_set(ad->win, ELM_WIN_INDICATOR_OPAQUE);
-	evas_object_size_hint_weight_set(ad->conform, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-	elm_win_resize_object_add(ad->win, ad->conform);
-	evas_object_show(ad->conform);
-
-	/* Label */
-	/* Create an actual view of the base gui.
-	   Modify this part to change the view. */
-	ad->label = elm_label_add(ad->conform);
-	elm_object_text_set(ad->label, "<align=center>Hello Tizen</align>");
-	evas_object_size_hint_weight_set(ad->label, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-	elm_object_content_set(ad->conform, ad->label);
-
-	/* Show window after base gui is set up */
-	evas_object_show(ad->win);
+    // Create a button
+    Evas_Object *bt = elm_button_add(display);
+    elm_object_text_set(bt, name);
+    evas_object_smart_callback_add(bt, "clicked", (Evas_Smart_Cb) cb, data);
+    evas_object_size_hint_weight_set(bt, EVAS_HINT_EXPAND, 0.0);
+    evas_object_size_hint_align_set(bt, EVAS_HINT_FILL, EVAS_HINT_FILL);
+    elm_box_pack_end(display, bt);
+    evas_object_show(bt);
+    return bt;
 }
 
-static bool
-app_create(void *data)
+static void _btn_clear_cb(void *data, Evas_Object *btn, void *ev)
 {
-	/* Hook to take necessary actions before main event loop starts
-		Initialize UI resources and application's data
-		If this function returns true, the main loop of application starts
-		If this function returns false, the application is terminated */
-	appdata_s *ad = data;
-
-	create_base_gui(ad);
-
-	return true;
+    elm_entry_entry_set(GLOBAL_DEBUG_BOX, "");
 }
 
-static void
-app_control(app_control_h app_control, void *data)
+Evas_Object *_create_new_cd_display(appdata_s *ad, char *name, void *cb, Evas_Object *conform)
 {
-	/* Handle the launch request. */
+    Evas_Object *box = elm_box_add(conform);
+    elm_object_content_set(conform, box);
+
+    elm_box_horizontal_set(box, EINA_FALSE);
+    evas_object_size_hint_align_set(box, EVAS_HINT_FILL, EVAS_HINT_FILL);
+    evas_object_size_hint_weight_set(box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+    evas_object_show(box);
+
+    // Create a box for adding content
+    Evas_Object *bbox = elm_box_add(box);
+    Evas_Coord padding_between_buttons = 3;
+    elm_box_padding_set(bbox, 0, padding_between_buttons);
+    elm_box_horizontal_set(bbox, EINA_FALSE);
+    evas_object_size_hint_align_set(bbox, EVAS_HINT_FILL, EVAS_HINT_FILL);
+    evas_object_size_hint_weight_set(bbox, EVAS_HINT_EXPAND, 0.0);
+    elm_box_pack_end(box, bbox);
+    evas_object_show(bbox);
+
+
+    return box;
 }
 
-static void
-app_pause(void *data)
+static void create_base_gui(appdata_s *ad)
 {
-	/* Take necessary actions when application becomes invisible. */
+    // Required to display the camera preview correctly (without any rotation).
+    elm_config_accel_preference_set("opengl");
+
+    // Setting the window
+    ad->win = elm_win_util_standard_add(PACKAGE, PACKAGE);
+    elm_win_conformant_set(ad->win, EINA_TRUE);
+    elm_win_autodel_set(ad->win, EINA_TRUE);
+    elm_win_indicator_mode_set(ad->win, ELM_WIN_INDICATOR_SHOW);
+    elm_win_indicator_opacity_set(ad->win, ELM_WIN_INDICATOR_OPAQUE);
+
+    /* Create conformant */
+    Evas_Object *conform = elm_conformant_add(ad->win);
+    evas_object_size_hint_weight_set(conform, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+    elm_win_resize_object_add(ad->win, conform);
+//    evas_object_show(conform);
+
+
+    // Fill the list with items
+    Evas_Object* mainBox = create_buttons_in_main_window(ad, conform);
+
+    //viren+
+    elm_object_content_set(conform, mainBox);
+    evas_object_show(conform);
+
+    startCamera(ad);
+    //viren-
+
+    // Show the window after base gui is set up
+    evas_object_show(ad->win);
 }
 
-static void
-app_resume(void *data)
+static bool app_create(void *data)
 {
-	/* Take necessary actions when application becomes visible. */
+    /*
+     * Hook to take necessary actions before main event loop starts
+     * Initialize UI resources and application's data
+     * If this function returns true, the main loop of application starts
+     * If this function returns false, the application is terminated
+     */
+    create_base_gui((appdata_s *)data);
+
+    return true;
 }
 
-static void
-app_terminate(void *data)
+static void app_terminate(void *data)
 {
-	/* Release all resources. */
+    camera_pop_cb();
 }
 
-static void
-ui_app_lang_changed(app_event_info_h event_info, void *user_data)
+int main(int argc, char *argv[])
 {
-	/*APP_EVENT_LANGUAGE_CHANGED*/
+    appdata_s ad;
+    memset(&ad, 0x00, sizeof(appdata_s));
 
-	int ret;
-	char *language;
+    ui_app_lifecycle_callback_s event_callback;
+    memset(&event_callback, 0x00, sizeof(ui_app_lifecycle_callback_s));
 
-	ret = app_event_get_language(event_info, &language);
-	if (ret != APP_ERROR_NONE) {
-		dlog_print(DLOG_ERROR, LOG_TAG, "app_event_get_language() failed. Err = %d.", ret);
-		return;
-	}
+    event_callback.create = app_create;
+    event_callback.terminate = app_terminate;
 
-	if (language != NULL) {
-		elm_language_set(language);
-		free(language);
-	}
+    int error_code = ui_app_main(argc, argv, &event_callback, &ad);
+    if (error_code != APP_ERROR_NONE)
+        DLOG_PRINT_ERROR("ui_app_main()", error_code);
+
+    return error_code;
 }
 
-static void
-ui_app_orient_changed(app_event_info_h event_info, void *user_data)
-{
-	/*APP_EVENT_DEVICE_ORIENTATION_CHANGED*/
-	return;
-}
-
-static void
-ui_app_region_changed(app_event_info_h event_info, void *user_data)
-{
-	/*APP_EVENT_REGION_FORMAT_CHANGED*/
-}
-
-static void
-ui_app_low_battery(app_event_info_h event_info, void *user_data)
-{
-	/*APP_EVENT_LOW_BATTERY*/
-}
-
-static void
-ui_app_low_memory(app_event_info_h event_info, void *user_data)
-{
-	/*APP_EVENT_LOW_MEMORY*/
-}
-
-int
-main(int argc, char *argv[])
-{
-	appdata_s ad = {0,};
-	int ret = 0;
-
-	ui_app_lifecycle_callback_s event_callback = {0,};
-	app_event_handler_h handlers[5] = {NULL, };
-
-	event_callback.create = app_create;
-	event_callback.terminate = app_terminate;
-	event_callback.pause = app_pause;
-	event_callback.resume = app_resume;
-	event_callback.app_control = app_control;
-
-	ui_app_add_event_handler(&handlers[APP_EVENT_LOW_BATTERY], APP_EVENT_LOW_BATTERY, ui_app_low_battery, &ad);
-	ui_app_add_event_handler(&handlers[APP_EVENT_LOW_MEMORY], APP_EVENT_LOW_MEMORY, ui_app_low_memory, &ad);
-	ui_app_add_event_handler(&handlers[APP_EVENT_DEVICE_ORIENTATION_CHANGED], APP_EVENT_DEVICE_ORIENTATION_CHANGED, ui_app_orient_changed, &ad);
-	ui_app_add_event_handler(&handlers[APP_EVENT_LANGUAGE_CHANGED], APP_EVENT_LANGUAGE_CHANGED, ui_app_lang_changed, &ad);
-	ui_app_add_event_handler(&handlers[APP_EVENT_REGION_FORMAT_CHANGED], APP_EVENT_REGION_FORMAT_CHANGED, ui_app_region_changed, &ad);
-
-	ret = ui_app_main(argc, argv, &event_callback, &ad);
-	if (ret != APP_ERROR_NONE) {
-		dlog_print(DLOG_ERROR, LOG_TAG, "app_main() is failed. err = %d", ret);
-	}
-
-	return ret;
-}
